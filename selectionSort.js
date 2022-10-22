0e578e6cd304afdf08ea3091a9761c8f36f2a6fb
Kamila Jesus <kamilajesusdev@gmail.com>
2022-10-21 22:57:08 -0300

    Ordenação com Selection Sort

---
+++ selectionSort.js
@@ -0,0 +1,16 @@
+const livros = require('./listaLivros');
+const menorValor = require('./menorValor');
+
+for(let atual = 0; atual < livros.length; atual++){
+  let menor = menorValor(livros, atual)
+
+  let livroAtual = livros[atual];
+  console.log('Livro atual', livros[atual])
+  let livrosMenorPreco = livros[menor];
+  console.log('Livro menor preço', livros[menor])
+
+  livros[atual] = livrosMenorPreco
+  livros[menor] = livroAtual
+}
+
+console.log(livros)
